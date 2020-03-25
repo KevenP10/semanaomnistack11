3f6f3090a84760d0c44a360661fad3f23a5a699a
Keven Pacheco <kevenpacheco2000@gmail.com>
2020-03-25 20:26:07 -0300

    front-end do app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Routes from './routes';
+import './global.css';
+
+// JSX (JavaScript XML), ou seja HTML dentro do JS
+//Para colocar um JS em um HTML, usa as "{}"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Keven Lindão!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Routes />
   );
 }
 
